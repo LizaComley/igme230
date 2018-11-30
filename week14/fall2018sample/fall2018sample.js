a901ef62f1530e0b490978345b17bbf264d4031c
Liza Comley <lizacomley@lizas-mbp.wireless.rit.edu>
2018-11-29 20:01:09 -0500

    practical practice

--- week14/fall2018sample/fall2018sample.js
+++ week14/fall2018sample/fall2018sample.js
@@ -1,1 +1,16 @@
 /* Don't use <script> tags in a linked js file! */
+src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"
+
+let select = ("content1.txt")  // sets default verse element
+$("#choose-content").val(select); // changes menu option to default
+$("#content").load(select);   // retrieves only default element
+
+$("#choose-content").change(function() {
+    select = $(this).val();
+    $("#content").load(select); 
+});
+
+
+$("nav").click(function() {
+    $(".submenu").toggle();
+});
