3968e4f854805280b550f3a472a5fa3a6248571b
Liza Comley <lizacomley@lizas-mbp.wireless.rit.edu>
2018-11-30 12:17:23 -0500

    commenting

--- week14/fall2018sample/fall2018sample.js
+++ week14/fall2018sample/fall2018sample.js
@@ -1,17 +1,28 @@
 /* Don't use <script> tags in a linked js file! */
 src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"
 
-let select = ("content1.txt")  // sets default verse element
-$("#choose-content").val(select); // changes menu option to default
-$("#content").load(select);   // retrieves only default element
 
+/************************************************************************
+*                        AJAX Content Loading                           *
+*************************************************************************/
+
+let selection = ("content1.txt")  // sets default content to be displayed
+$("#choose-content").val(selection); // changes menu option to the default
+$("#content").load(selection);   // retrieves only default element
+
+/* when the content selection is changed
+change the value of selection and load that content */
 $("#choose-content").change(function() {
-    select = $(this).val();
-    $("#content").load(select); 
+    selection = $(this).val();
+    $("#content").load(selection); 
 });
 
+/************************************************************************
+*                   Content Menu Functionality                          *
+*************************************************************************/
 
-
+/* when a menubox is clicked,
+finds the submenu for the selected and toggles it's display on and off */
 $(".menubox").click(function() {
-    $(this).find(".submenu").toggle();
+    $(this).find(".submenu").slideToggle(); /* slideToggle() - toggle function w. slide animation */
 });
