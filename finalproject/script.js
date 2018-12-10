ba736f09d2e8ea06ab39c7e44fbb0d8fe72ef25c
Liza Comley <lizacomley@lizas-mbp.wireless.rit.edu>
2018-12-09 23:17:55 -0500

    progressssssss

---
+++ finalproject/script.js
@@ -0,0 +1,15 @@
+src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"
+
+$(".p1").hover(function() {
+    $(this).find(".description").toggle(); 
+});
+$(".p2").hover(function() {
+    $(this).find(".description").toggle(); 
+});
+$(".p3").hover(function() {
+    $(this).find(".description").toggle(); 
+});
+$(".p4").hover(function() {
+    $(this).find(".description").toggle(); 
+});
+
